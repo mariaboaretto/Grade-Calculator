3b8ae1bce158c2b2e0e1234b400ffceab0a73e28
Maria Boaretto <mariaboarettocampos@gmail.com>
2020-09-30 20:28:17 -0300

    Adding function to get user input

--- gradecalculator.c
+++ gradecalculator.c
@@ -4,80 +4,25 @@
 
 void inputs()
 {
-    char firstGrade[10], secondGrade[10], thirdGrade[10], fourthGrade[10], fifthGrade[10];
-    int gOne, gTwo, gThree, gFour, gFive;
-    int size = 5;
+    int grades[5];
+    int classSize = 5;
+    char *student[10] = {"1º", "2º", "3º", "4º", "5º"};
 
-    for (int i = 0; i <= size; i++)
+    for (int i = 0; i < classSize; i++)
     {
-        switch (i)
+        printf("\nInsira a nota do %s aluno:\n", student[i]);
+        scanf("%d", &grades[i]);
+        printf("%d", grades[i]);
+        if (grades[i] < 1 || grades[i] > 10)
         {
-        case 1:
-            printf("\nInsira a nota do primeiro aluno: ");
-            scanf("%s", &firstGrade);
-            gOne = atoi(firstGrade);
-            break;
-        case 2:
-            printf("\nInsira a nota do segundo aluno: ");
-            scanf("%s", &secondGrade);
-            gTwo = atoi(secondGrade);
-            break;
-        case 3:
-            printf("\nInsira a nota do terceiro aluno: ");
-            scanf("%s", &thirdGrade);
-            gThree = atoi(thirdGrade);
-            break;
-        case 4:
-            printf("\nInsira a nota do quarto aluno: ");
-            scanf("%s", &fourthGrade);
-            gFour = atoi(fourthGrade);
-            break;
-        case 5:
-            printf("\nInsira a nota do quinto aluno: ");
-            scanf("%s", &fifthGrade);
-            gFive = atoi(fifthGrade);
-            break;
-
-        default:
+            printf("\nNota inválida. Insira apenas números de 1 a 10.\n");
             break;
         }
     }
-
-    printf("\n%d, %d, %d, %d, %d\n", gOne, gTwo, gThree, gFour, gFive);
-}
-
-int firstInput()
-{
-    char firstGrade[10];
-    int gradeOne;
-
-    do
-    {
-        printf("\nInsira a nota do primeiro aluno: ");
-        scanf("%s", &firstGrade);
-        gradeOne = atoi(firstGrade);
-
-        if (gradeOne < 1 || gradeOne > 10)
-        {
-            printf("\nNota inválida. Insira apenas números de 1 a 10.\n");
-        }
-
-    } while (gradeOne < 1 || gradeOne > 10);
-
-    return gradeOne;
 }
 
 int main()
 {
-    /*
-    int gradeOne, gradeTwo, gradeThree, gradeFour, gradeFive;
-    gradeOne = firstInput();
-    gradeTwo = secInput();
-    gradeThree = thirdInput();
-    gradeFour = fourthInput();
-    gradeFive = fifthInput();
-    int Grades[5] = {gradeOne, gradeTwo, gradeThree, gradeFour, gradeFive};
-    */
     inputs();
 
     return 0;
