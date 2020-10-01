b6510434389ff449e7b7df47ae707c00dfc31a19
Maria Boaretto <mariaboarettocampos@gmail.com>
2020-10-01 20:00:14 -0300

    Adding functions to get min and max grades

--- gradecalculator.c
+++ gradecalculator.c
@@ -2,17 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 
-void inputs()
+void inputs(int *grades, int classSize)
 {
-    int grades[5];
-    int classSize = 5;
-    char *student[10] = {"1º", "2º", "3º", "4º", "5º"};
-
     for (int i = 0; i < classSize; i++)
     {
-        printf("\nInsira a nota do %s aluno:\n", student[i]);
+        printf("\nInsira a nota do %dº aluno:\n", i + 1);
         scanf("%d", &grades[i]);
-        printf("%d", grades[i]);
+
         if (grades[i] < 1 || grades[i] > 10)
         {
             printf("\nNota inválida. Insira apenas números de 1 a 10.\n");
@@ -21,9 +17,46 @@ void inputs()
     }
 }
 
+void maxGrade(int *grades, int classSize)
+{
+    int currentMax = grades[0];
+
+    for (int i = 0; i < classSize; i++)
+    {
+        if (grades[i] > currentMax)
+        {
+            currentMax = grades[i];
+        }
+    }
+    printf("\nA maior nota é: %d\n", currentMax);
+}
+
+void minGrade(int *grades, int classSize)
+{
+    int currentMin = grades[0];
+
+    for (int i = 0; i < classSize; i++)
+    {
+        if (grades[i] < currentMin)
+        {
+            currentMin = grades[i];
+        }
+    }
+    printf("\nA menor nota é: %d\n", currentMin);
+}
+
+void classAverage(int *grades)
+{
+    
+}
+
 int main()
 {
-    inputs();
+    int grades[10];
+    int classSize = 10;
+    inputs(grades, classSize);
+    maxGrade(grades, classSize);
+    minGrade(grades, classSize);
 
     return 0;
 }
